7016066a76400c4c1b13bb70715700f416ee33a5
bbayanian <babak.bayanian@gmail.com>
2023-05-28 10:17:58 +0330

    remve unused

--- Attence_tejarat1.31.py
+++ Attence_tejarat1.31.py
@@ -5,12 +5,9 @@ import contextlib
 with contextlib.redirect_stdout(None):
     import pygame
 import pygame.freetype
-#import testding_2py
-#import schedule
+
 import pygwidgets
-import time
-#import Cal_borders
-#import TEDAD_RUZ
+
 from attencee import ALL
 import arabic_reshaper
 from bidi.algorithm import get_display
@@ -58,17 +55,10 @@ class Button:
                     
                     global p
                     p=0
-                    #print(p)
-                    '''[stringJOLFA, stringJOLFA_tell, stringNorduz, stringnoruz_tell, stringAIR, stringAIR_tell, stringASTARA, 
-                     stringASTARA_tell, stringBILE, stringbile_tell,stringSETAD,stringSETAD_tell] =ALL()'''
-                    #print(stringSETAD)
 
 
 
-'''def calling_ding():
-    return [stringJOLFA, stringJOLFA_tell, stringNorduz, stringnoruz_tell, stringAIR, stringAIR_tell, stringASTARA, stringASTARA_tell, stringBILE, stringbile_tell,stringSETAD,stringSETAD_tell]
-'''
-        
+  
 
 global stringJOLFA
 global stringNorduz
@@ -109,23 +99,10 @@ def TEXT_Manabe():
     
     oMessageText1 = pygwidgets.DisplayText(window, (95,J_Y),'Jolfa', 
                                     fontSize=36, textColor=ORANGE)
-    '''
-    oMessageText2 = pygwidgets.DisplayText(window, (J_X, J_Y+40),'NO. Per DAY', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageText3 = pygwidgets.DisplayText(window, (J_X, J_Y+70),'NO. Per Month', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageText4 = pygwidgets.DisplayText(window, (J_X, J_Y+100),'CANCEL by Admin', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageText5 = pygwidgets.DisplayText(window, (J_X, J_Y+130),'CANCEL by SYSTEM', 
-                                    fontSize=fon_S, textColor=WHITE)
-                                    '''
+
+                                  
     oMessageText1.draw()
-    #oMessageText2.draw()
-    #oMessageText3.draw()
-    #oMessageText4.draw()
-    #oMessageText5.draw()
-    
-    ###ASTARA
+  
     J_X=500
     J_Y=20
     fon_S=15
@@ -133,37 +110,15 @@ def TEXT_Manabe():
     
     oMessageTextASTARA1 = pygwidgets.DisplayText(window, (500,J_Y),'ASTARA', 
                                     fontSize=36, textColor=ORANGE)
-    '''oMessageTextASTARA2 = pygwidgets.DisplayText(window, (J_X, J_Y+40),'NO. Per DAY', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageTextASTARA3 = pygwidgets.DisplayText(window, (J_X, J_Y+70),'NO. Per Month', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageTextASTARA4 = pygwidgets.DisplayText(window, (J_X, J_Y+100),'CANCEL by Admin', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageTextASTARA5 = pygwidgets.DisplayText(window, (J_X, J_Y+130),'CANCEL by SYSTEM', 
-                                    fontSize=fon_S, textColor=WHITE)'''
+   
     oMessageTextASTARA1.draw()
-    #oMessageTextASTARA2.draw()
-    #oMessageTextASTARA3.draw()
-    #oMessageTextASTARA4.draw()
-    #oMessageTextASTARA5.draw()
-    
-    ##norduz
+   
     J_X=30
     J_Y=200
     oMessageText6 = pygwidgets.DisplayText(window, (80,J_Y),'NORDUZ', 
                                     fontSize=36, textColor=ORANGE)
-    '''oMessageText7 = pygwidgets.DisplayText(window, (J_X, J_Y+40),'NO. Per DAY', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageText8 = pygwidgets.DisplayText(window, (J_X, J_Y+70),'NO. Per Month', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageText9 = pygwidgets.DisplayText(window, (J_X, J_Y+100),'CANCEL by Admin', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageText10 = pygwidgets.DisplayText(window, (J_X, J_Y+130),'CANCEL by SYSTEM', 
-                                    fontSize=fon_S, textColor=WHITE)'''
-    #oMessageText10.draw()
-    #oMessageText9.draw()
-    #oMessageText8.draw()
-    #oMessageText7.draw()
+  
+    
     oMessageText6.draw()
     
     ##bil
@@ -171,76 +126,34 @@ def TEXT_Manabe():
     J_Y=20
     oMessageText11 = pygwidgets.DisplayText(window, (J_X,J_Y),'BILESAVAR', 
                                     fontSize=36, textColor=ORANGE)
-    '''oMessageText12 = pygwidgets.DisplayText(window, (J_X, J_Y+40),'NO. Per DAY', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageText13 = pygwidgets.DisplayText(window, (J_X, J_Y+70),'NO. Per Month', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageText14 = pygwidgets.DisplayText(window, (J_X, J_Y+100),'CANCEL by Admin', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageText15 = pygwidgets.DisplayText(window, (J_X, J_Y+130),'CANCEL by SYSTEM', 
-                                    fontSize=fon_S, textColor=WHITE)'''
+    
     oMessageText11.draw()
-    #oMessageText12.draw()
-    #oMessageText13.draw()
-    #oMessageText14.draw()
-    #oMessageText15.draw()
-    
-    
-    
     
     ##AIRPORT
     J_X=500
     J_Y=200
     oMessageTextAT1 = pygwidgets.DisplayText(window, (J_X,J_Y),'AIRPORT', 
                                     fontSize=36, textColor=ORANGE)
-    '''oMessageTextAT2 = pygwidgets.DisplayText(window, (J_X, J_Y+40),'NO. Per DAY', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageTextAT3 = pygwidgets.DisplayText(window, (J_X, J_Y+70),'NO. Per Month', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageTextAT4 = pygwidgets.DisplayText(window, (J_X, J_Y+100),'CANCEL by Admin', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageTextAT5 = pygwidgets.DisplayText(window, (J_X, J_Y+130),'CANCEL by SYSTEM', 
-                                    fontSize=fon_S, textColor=WHITE)'''
+   
     oMessageTextAT1.draw()
     oMessageTextASTARA9 = pygwidgets.DisplayText(window, (500,380),'SETAD', 
                                     fontSize=36, textColor=ORANGE)
     oMessageTextASTARA9.draw()
-    #oMessageTextAT2.draw()
-    #oMessageTextAT3.draw()
-    #oMessageTextAT4.draw()
-    #oMessageTextAT5.draw()
-    
-    '''
-    ####hame marzha
-    J_X=500
-    J_Y=390
-    
-    oMessageTextA1 = pygwidgets.DisplayText(window, (J_X,J_Y),'ALL', 
-                                    fontSize=36, textColor=ORANGE)
-    oMessageTextA2 = pygwidgets.DisplayText(window, (J_X, J_Y+40),'NO. Per DAY', 
-                                    fontSize=fon_S, textColor=WHITE)
-    oMessageTextA3 = pygwidgets.DisplayText(window, (J_X, J_Y+70),'NO. Per Month', 
-                                    fontSize=fon_S, textColor=WHITE)'''
-    
-    #oMessageTextA1.draw()
-    #oMessageTextA2.draw()
-    #oMessageTextA3.draw()
-    #text_rect = ft_font.get_rect('ty()')
-    #text_rect.center = window.get_rect().center
-    #ft_font.render_to(window, text_rect.topleft, str(jolfa_PD), (255, 0, 0))
-#schedule.every(1).minutes.do(text_ADAD)
+   
 '''logoI=pygame.image.load('logo Tejarat_Arman.png').convert()
 
 #logoIRECT=logoI.get_rect()
 window.blit(logoI, (30,30))
 '''
-
+TEXTref="بارگذاری دوباره"
+reshaped_text = arabic_reshaper.reshape(TEXTref)
+refreshText= (get_display(reshaped_text))
 button1 = Button(
-    "REFRESH",
+    refreshText,
     (280, 500),
     font=30,
     bg="blue",
-    feedback="REFRESH")
+    feedback=refreshText)
 run = True
 pygame.display.set_caption('Tejarat Electronic Arman Monitoring')
 p==0
@@ -254,16 +167,14 @@ while run:
         [NAME_UK_jolfa, NAME_jolfa,NAME_UK_norduz, NAME_norduz, NAME_UK_AIR, NAME_AIR,NAME_UK_ASTARA, NAME_ASTARA,NAME_UK_BILE, NAME_BILE,stringSETAD,stringSETAD_tell]=ALL()
         p=1
         TIME_REF= datetime.datetime.now()
-        #print(stringSETAD,NAME_UK_AIR)
-        secTime=datetime.timedelta(0,10)+datetime.datetime.now()
+        secTime=datetime.timedelta(0,300)+datetime.datetime.now()
 
     
     if datetime.datetime.now()>secTime:
-        print("good")
         TIME_REF=datetime.datetime.now()
         [NAME_UK_jolfa, NAME_jolfa,NAME_UK_norduz, NAME_norduz, NAME_UK_AIR, NAME_AIR,NAME_UK_ASTARA, NAME_ASTARA,NAME_UK_BILE, NAME_BILE,stringSETAD,stringSETAD_tell]=ALL()
 
-        secTime=datetime.timedelta(0,10)+datetime.datetime.now()
+        secTime=datetime.timedelta(0,300)+datetime.datetime.now()
         
             
    
@@ -286,18 +197,12 @@ while run:
     
 
     
-    ##window.blit(logoI, (20+PMX+30,180+PMY+40))
 
     TEXT_Manabe()
     J_X=160
     J_Y=-50
     S_F=30
-    ##
-    
-    #[NAME_UK_jolfa, NAME_jolfa]=testding_2py.JOLFA()
-    
-    #print(stringSETAD)
-    ##jolfa
+   
     
     oMessageText105 = pygwidgets.DisplayText(window, (J_X-80, J_Y+150),fontName='BNazsanin.ttf',value=NAME_UK_jolfa, 
                                     fontSize=S_F, textColor=WHITE)
@@ -310,11 +215,9 @@ while run:
     J_Y=100
   
     
-    #[NAME_UK_norduz, NAME_norduz]=testding_2py.norduz()
 
     oMessageTextN4 = pygwidgets.DisplayText(window, (J_X-80, J_Y+150),fontName='BNazsanin.ttf',value=NAME_UK_norduz, 
                                     fontSize=S_F, textColor=WHITE)
-    #print(NAME_UK_norduz)
     
     oMessageTextN4.draw()
     #bil
@@ -324,7 +227,6 @@ while run:
     
     
     
-    #[NAME_UK_BILE, NAME_BILE]=testding_2py.BILE()
 
     oMessageTextB5 = pygwidgets.DisplayText(window, (J_X-80, J_Y+150),fontName='BNazsanin.ttf',value=NAME_UK_BILE, 
                                     fontSize=S_F, textColor=WHITE)
@@ -337,7 +239,6 @@ while run:
     J_Y=100
     
     
-    #[NAME_UK_AIR, NAME_AIR]=testding_2py.AIR()
 
     oMessageTextAP5 = pygwidgets.DisplayText(window, (J_X-80, J_Y+150),fontName='BNazsanin.ttf',value= NAME_UK_AIR, 
                                     fontSize=S_F, textColor=WHITE)
@@ -350,7 +251,6 @@ while run:
     J_Y=-50
     
    
-    #[NAME_UK_AIR, NAME_AIR]=testding_2py.ASTARA()
 
     oMessageTextAP5 = pygwidgets.DisplayText(window, (J_X-80, J_Y+150),fontName='BNazsanin.ttf',value=NAME_UK_ASTARA, 
                                     fontSize=S_F, textColor=WHITE)
@@ -358,7 +258,6 @@ while run:
   
     oMessageTextAP5.draw()
 
-    #print(len(stringSETAD))
     
     ###ALLLL
     J_X=600
@@ -366,11 +265,8 @@ while run:
     
     oMessageTextX = pygwidgets.DisplayText(window, (J_X, J_Y),fontName='BNazsanin.ttf',value=(stringSETAD), 
                                     fontSize=16, textColor=WHITE)
-    #oMessageTextY = pygwidgets.DisplayText(window, (J_X, J_Y+70),stringSETAD, 
-     #                               fontSize=S_F, textColor=WHITE)
     
     oMessageTextX.draw()
-    #oMessageTextY.draw()
   
     oMessageTextref = pygwidgets.DisplayText(window, (20, J_Y+160),fontName='BNazsanin.ttf',value=TIME_REF, 
                                     fontSize=15, textColor=WHITE)
@@ -380,8 +276,7 @@ while run:
    
     
     pygame.display.flip()
-    #schedule.run_pending()
-    #time.sleep(10)
+    
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
