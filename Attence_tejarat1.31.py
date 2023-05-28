4f9645a508f7a1d4f25eaa3ddd794aeb575f7677
bbayanian <babak.bayanian@gmail.com>
2023-05-28 13:02:30 +0330

    1.32

--- Attence_tejarat1.31.py
+++ Attence_tejarat1.31.py
@@ -13,15 +13,18 @@ import arabic_reshaper
 from bidi.algorithm import get_display
 from pygame.locals import *
 import datetime
-global TIME_REF
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial", 20)
-global p 
-p=0
+
 global stringSETAD
 global stringAIR
+global stringJOLFA
+global stringNorduz
+global stringASTARA
+global stringBILE
+global p
+global TIME_REF
+
+
+
 class Button:
     """Create a button, then blit the surface in the while loop"""
 
@@ -44,7 +47,7 @@ class Button:
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
 
     def show(self):
-        screen.blit(button1.surface, (self.x, self.y))
+        window.blit(button1.surface, (self.x, self.y))
 
     def click(self, event):
         x, y = pygame.mouse.get_pos()
@@ -58,38 +61,26 @@ class Button:
 
 
 
-  
-
-global stringJOLFA
-global stringNorduz
-global stringASTARA
-global stringBILE
-
-
-    
 pygame.init()
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((710, 560))
 
 clock = pygame.time.Clock()
 # 2 - Define constants
+font = pygame.font.SysFont("Arial", 20)
+
 BLACK = (0, 0, 0)
-GRAY = (128, 128, 128)
 WHITE = (255, 255, 255)
 ORANGE=(245,110,20)
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
-FRAMES_PER_SECOND = 30
-N_PIXELS_PER_FRAME = 3
-BALL_WIDTH_HEIGHT = 100
-RED=(255,0,0)
+
+
+p=0
+o=1
+
 
 ft_font = pygame.freetype.SysFont('Times New Roman', 20)
 
 background = pygame.Surface(window.get_size())
 
-
-
-o=1
 
 def TEXT_Manabe():
     ##jolfa
@@ -154,11 +145,10 @@ button1 = Button(
     font=30,
     bg="blue",
     feedback=refreshText)
-run = True
 pygame.display.set_caption('Tejarat Electronic Arman Monitoring')
-p==0
+
     
-   
+run = True
 while run:
     clock.tick(30)
     pygame. draw. polygon(window, (0,0,0), ((20,500),(20,560),(240,560),(240,500)))
@@ -175,12 +165,10 @@ while run:
         [NAME_UK_jolfa, NAME_jolfa,NAME_UK_norduz, NAME_norduz, NAME_UK_AIR, NAME_AIR,NAME_UK_ASTARA, NAME_ASTARA,NAME_UK_BILE, NAME_BILE,stringSETAD,stringSETAD_tell]=ALL()
 
         secTime=datetime.timedelta(0,300)+datetime.datetime.now()
-        
-            
-   
+                  
     PMX=230
     PMY=180
-
+    ###making polygon 
     pygame.draw.polygon(window,(0,68,255),((20,20),(230,20),(230,180),(20,180)))
     pygame.draw.polygon(window,(0,68,255),((20+PMX,20+PMY),(230+PMX,20+PMY),(230+PMX,180+PMY),(20+PMX,180+PMY)))
     pygame.draw.polygon(window,(0,68,255),((20,20+PMY),(230,20+PMY),(230,180+PMY),(20,180+PMY)))
@@ -197,7 +185,7 @@ while run:
     
 
     
-
+    #show the background and Text
     TEXT_Manabe()
     J_X=160
     J_Y=-50
@@ -225,8 +213,6 @@ while run:
     J_Y=-50
    
     
-    
-    
 
     oMessageTextB5 = pygwidgets.DisplayText(window, (J_X-80, J_Y+150),fontName='BNazsanin.ttf',value=NAME_UK_BILE, 
                                     fontSize=S_F, textColor=WHITE)
@@ -240,7 +226,7 @@ while run:
     
     
 
-    oMessageTextAP5 = pygwidgets.DisplayText(window, (J_X-80, J_Y+150),fontName='BNazsanin.ttf',value= NAME_UK_AIR, 
+    oMessageTextAP5 = pygwidgets.DisplayText(window, (J_X-70, J_Y+150),fontName='BNazsanin.ttf',value= NAME_UK_AIR, 
                                     fontSize=S_F, textColor=WHITE)
     
     oMessageTextAP5.draw()
